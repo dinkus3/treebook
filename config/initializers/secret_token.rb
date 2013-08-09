094e809a8d5d5961e78d15e715ded842d048afca
Cole Russell <dinkus@gmail.com>
2013-08-08 19:56:13 -0700

    Created a rils application and added status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treebook::Application.config.secret_token = 'cf06d33f461996ceacf514873f4c3da093200b5cc73e7dfceb110ec08b9971868f365299b05f5c99fbc028e73ff304f865df22dbe25fc7b3e1396447fc7725f5'
